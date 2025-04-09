6bb1b5f853fbbe789ec2b1f21c92e787f65f44af
sgrabinski <sgrabinski@parasoft.com>
2025-04-09 09:39:39 +0200

    new2

--- timer.c
+++ timer.c
@@ -37,7 +37,7 @@ void uninit_timer()
  */
 struct timer_record* query_user()
 {
-    int start_h, start_m, end_h, end_m;
+    int start_h, start_m, end_h; //after suppresion is 200 violations, remove end_m, it should be 199
     struct timer_record* the_record;
     time_t timer;
     struct tm* tm_tmp;
